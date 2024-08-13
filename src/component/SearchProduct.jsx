f7309c01d4f54f91c5dd7948330fd73fde611b5d
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-13 23:56:12 +0530

    Changes8

--- src/component/SearchProduct.jsx
+++ src/component/SearchProduct.jsx
@@ -12,14 +12,13 @@ const SearchProduct=()=>{
     const [mydata,setMydata]=useState([]);
     const dispatch= useDispatch();
 
-    // const handleChange=(e)=>{
-    //     setVal(e.target.value);
-    //     let api=`http://localhost:3000/product`;
-    //     axios.get(api).then((res)=>{
-    //         setMydata(res.data);
-    //     })
-    // }
-    
+    const handleChange=(e)=>{
+        setVal(e.target.value);
+        let api=`http://localhost:3000/product`;
+        axios.get(api).then((res)=>{
+            setMydata(res.data);
+        })
+    }
 
     const DataCart=(pid,nm,img,desc,price)=>{
         dispatch(addtoCart({id:pid,name:nm,images:img,description:desc,qnty:1,price:price}))
