20024d3bf98ba11a1cd06562f3b8f2d94fa3765a
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-18 14:38:35 +0530

    Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -41,7 +41,36 @@ const Top=()=>{
             <Nav.Link as={Link} to="searchproduct" style={{color:"black",fontSize:"20px",marginLeft:"10px"}}>
             Search
             </Nav.Link>
+{/* ============================= Dropdown Section ============================ */}
+
+<Navbar expand="lg">
+      <Container fluid>
+        
+        <Navbar.Toggle aria-controls="navbar-dark-example" />
+        <Navbar.Collapse id="navbar-dark-example">
+          <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    {/* ========================= Close Dropdown ====================== */}
+     </Nav>
 
           <div>
             <TiShoppingCart style={{fontSize:"30px",color:"black",marginRight:"25px"}} onClick={changeNavigate} />
