2e526a6ea959fcafd22cf47ce7d1f27f82071ae6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-04 23:54:08 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -69,20 +69,7 @@
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div> <br />
-
-    </div>
     
-    </Container>
-    <Footer/>
-    </>
-  ) */}
-//}
-//export default App; */}
 
 
 import { BrowserRouter,Routes,Route } from "react-router-dom"
